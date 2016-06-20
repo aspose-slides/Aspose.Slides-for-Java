ead1f7ec6c67ea245a58dd69eab6503f9585100f
Aspose.Slides Examples, Plugins and Showcases <slides@aspose.com>
2016-06-20 10:00:49 +0500

    Update OpeningAPresentation.java

--- Examples/src/main/java/com/aspose/slides/examples/Presentation/Opening/OpeningAPresentation.java
+++ Examples/src/main/java/com/aspose/slides/examples/Presentation/Opening/OpeningAPresentation.java
@@ -8,10 +8,9 @@ public class OpeningAPresentation {
 	public static final void main(String[] args) {
 		
 		// The path to the documents directory.
-        String dataDir = Utils.getDataDir(OpeningAPresentation.class);
+        	String dataDir = Utils.getDataDir(OpeningAPresentation.class);
         
-		// Opening the presentation file by passing the file path to the
-		// constructor of Presentation class
+		// Opening the presentation file by passing the file path to the constructor of Presentation class
 		Presentation pres = new Presentation(dataDir + "Presentation.pptx");
 
 		// Printing the total number of slides present in the presentation
