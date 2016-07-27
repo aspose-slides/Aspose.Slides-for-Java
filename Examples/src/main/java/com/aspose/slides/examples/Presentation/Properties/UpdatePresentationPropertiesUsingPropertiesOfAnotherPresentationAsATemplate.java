d75d003c1c38dc4fb9875a55bd984a0f2c98dca5
“sohail-aspose” <email@aspose.com>
2016-07-27 19:13:12 +0500

    Aspose.Slides v16.6.0 Examples

---
+++ Examples/src/main/java/com/aspose/slides/examples/Presentation/Properties/UpdatePresentationPropertiesUsingPropertiesOfAnotherPresentationAsATemplate.java
@@ -0,0 +1,42 @@
+package com.aspose.slides.examples.Presentation.Properties;
+
+import com.aspose.slides.DocumentProperties;
+import com.aspose.slides.IDocumentProperties;
+import com.aspose.slides.IPresentationInfo;
+import com.aspose.slides.PresentationFactory;
+import com.aspose.slides.examples.Utils;
+
+public class UpdatePresentationPropertiesUsingPropertiesOfAnotherPresentationAsATemplate {
+
+	public static void main(String args[]) {
+		updateByTemplate();
+	}
+
+	private static void updateByTemplate() {
+		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(UpdatePresentationPropertiesUsingPropertiesOfAnotherPresentationAsATemplate.class);
+
+		DocumentProperties template;
+		IPresentationInfo info = PresentationFactory.getInstance().getPresentationInfo(dataDir + "Presentation.pptx");
+		template = (DocumentProperties) info.readDocumentProperties();
+
+		template.setAuthor("Template Author");
+		template.setTitle("Template Title");
+		template.setCategory("Template Category");
+		template.setKeywords("Keyword1, Keyword2, Keyword3");
+		template.setCompany("Our Company");
+		template.setComments("Created from template");
+		template.setContentType("Template Content");
+		template.setSubject("Template Subject");
+
+		updateByTemplate(dataDir + "doc1.pptx", template);
+		updateByTemplate(dataDir + "doc2.odp", template);
+		updateByTemplate(dataDir + "doc3.ppt", template);
+	}
+
+	private static void updateByTemplate(String path, IDocumentProperties template) {
+		IPresentationInfo toUpdate = PresentationFactory.getInstance().getPresentationInfo(path);
+		toUpdate.updateDocumentProperties(template);
+		toUpdate.writeBindedPresentation(path);
+	}
+}
---
+++ Examples/src/main/java/com/aspose/slides/examples/Slides/Shapes/AddingVideoFrameWithVideoFromWebSource.java
@@ -0,0 +1,42 @@
+package com.aspose.slides.examples.Slides.Shapes;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.aspose.slides.IVideoFrame;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.VideoPlayModePreset;
+import com.aspose.slides.examples.Utils;
+
+public class AddingVideoFrameWithVideoFromWebSource {
+
+	public static void main(String[] args) {
+		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(AddingVideoFrameWithVideoFromWebSource.class);
+
+		Presentation pres = new Presentation();
+		addVideoFromYouTube(pres, "Tj75Arhq5ho");
+		pres.save(dataDir + "out.pptx", SaveFormat.Pptx);
+	}
+
+	private static void addVideoFromYouTube(Presentation pres, String videoID) {
+		// add videoFrame
+		IVideoFrame videoFrame = pres.getSlides().get_Item(0).getShapes().addVideoFrame(10, 10, 427, 240, "https://www.youtube.com/embed/" + videoID);
+		videoFrame.setPlayMode(VideoPlayModePreset.Auto);
+
+		// load thumbnail
+		String thumbnailUri = "http://img.youtube.com/vi/" + videoID + "/hqdefault.jpg";
+		URL url;
+
+		try {
+			url = new URL(thumbnailUri);
+			videoFrame.getPictureFormat().getPicture().setImage(pres.getImages().addImage(url.openStream()));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
