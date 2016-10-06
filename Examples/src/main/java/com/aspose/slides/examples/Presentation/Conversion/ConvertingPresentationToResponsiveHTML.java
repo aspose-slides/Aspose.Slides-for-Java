2af06b024112ca2bd09a0023c43a5dc369efdd70
SOHAIL <msohailismail@gmail.com>
2016-10-06 15:23:56 +0500

    Added "Converting Presentation to Responsive HTML" example.

--- Examples/src/main/java/com/aspose/slides/examples/Presentation/Conversion/ConvertingPresentationToResponsiveHTML.java
+++ Examples/src/main/java/com/aspose/slides/examples/Presentation/Conversion/ConvertingPresentationToResponsiveHTML.java
@@ -12,13 +12,13 @@ public class ConvertingPresentationToResponsiveHTML {
 	private static final String dataDir = Utils.getSharedDataDir(ConvertingPresentationToResponsiveHTML.class) + "Conversion/";
 
 	public static void main(String[] args) {
-		Presentation pres = new Presentation(dataDir + "pres.pptx");
+		Presentation pres = new Presentation(dataDir + "presentation.pptx");
 		try {
 			ResponsiveHtmlController controller = new ResponsiveHtmlController();
 			HtmlOptions htmlOptions = new HtmlOptions();
 			htmlOptions.setHtmlFormatter(HtmlFormatter.createCustomFormatter(controller));
 
-			pres.save(dataDir + "pres.html", SaveFormat.Html, htmlOptions);
+			pres.save(dataDir + "pres_out.html", SaveFormat.Html, htmlOptions);
 		} finally {
 			if (pres != null)
 				pres.dispose();
