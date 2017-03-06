b321d10ab72c4045a687f6f8a20d99c5ccd2c241
Muhammad-Adnan-Ahmad <adnan.ahmad@aspose.com>
2017-03-06 23:24:19 +0500

    Added new features as per Aspose.Slides for Java 17.2.0 release
    Following Classes have been added:
    CheckSlidesComparison.java
    IdentifyingTheMergedCellsinTable.java

---
+++ Examples/src/main/java/com/aspose/slides/examples/Slides/Layout/CheckSlidesComparison.java
@@ -0,0 +1,42 @@
+package com.aspose.slides.examples.Slides.Layout;
+
+import com.aspose.slides.ISlide;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.examples.Utils;
+
+public class CheckSlidesComparison {
+
+	public static void main(String[] args) {
+   
+         //ExStart:CheckSlidesComparison
+		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(CheckSlidesComparison.class);
+
+		Presentation presentation1 = null;
+                 Presentation presentation2 = null;
+try {
+    presentation1 = new Presentation("SomePresentation1.pptx");
+    presentation2 = new Presentation("SomePresentation2.pptx");
+    for (int i = 0; i < presentation1.getMasters().size(); i++)
+    {
+        for (int j = 0; j < presentation2.getMasters().size(); j++)
+        {
+            if (presentation1.getMasters().get_Item(i).equals(presentation2.getMasters().get_Item(j)))
+      System.out.println("SomePresentation1 MasterSlide#" + i +" is equal to SomePresentation2 MasterSlide#" + j);
+        }
+    }
+} finally {
+    if (presentation1 != null) {
+        presentation1.dispose();
+        presentation1 = null;
+    }
+    if (presentation2 != null) {
+        presentation2.dispose();
+        presentation2 = null;
+    }
+}
+                  //ExStart:CheckSlidesComparison
+}
+
+}
---
+++ Examples/src/main/java/com/aspose/slides/examples/Slides/Table/IdentifyingTheMergedCellsinTable.java
@@ -0,0 +1,55 @@
+package com.aspose.slides.examples.Text;
+
+import com.aspose.slides.IAutoShape;
+import com.aspose.slides.IParagraph;
+import com.aspose.slides.IPortion;
+import com.aspose.slides.ISlide;
+import com.aspose.slides.ITextFrame;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.ShapeType;
+import com.aspose.slides.examples.Utils;
+
+public class IdentifyingTheMergedCellsinTable {
+
+	public static void main(String[] args) {
+                
+                ExStart:IdentifyingTheMergedCellsinTable
+
+		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(IdentifyingTheMergedCellsinTable.class);
+
+		Presentation pres = null;
+try
+{
+    pres = new Presentation(dataDir+"SomePresentationWithTable.pptx");
+
+    ITable table = (ITable)pres.getSlides().get_Item(0).getShapes().get_Item(0); // assuming that Slide#0.Shape#0 is a table
+    for (int i = 0; i < table.getRows().size(); i++)
+    {
+        for (int j = 0; j < table.getColumns().size(); j++)
+        {
+            ICell currentCell = table.getRows().get_Item(i).get_Item(j);
+            if (currentCell.isMergedCell())
+            {
+ System.out.println("Cell " + i + ";" + j +" is a part of merged cell with RowSpan=" + currentCell.getRowSpan() +
+                    " and ColSpan=" + currentCell.getColSpan() + " starting from Cell " + currentCell.getFirstRowIndex() +
+                    ";" + currentCell.getFirstColumnIndex() + ".");
+            }
+        }
+
+    }
+} finally {
+    if (pres != null) {
+        pres.dispose();
+        pres = null;
+    }
+}
+
+                        ExEnd:IdentifyingTheMergedCellsinTable
+
+
+                
+	}
+
+}
