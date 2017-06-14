af1681acdb7b9af46ff874466cac8ea0cc991a61
Muhammad-Adnan-Ahmad <adnan.ahmad@aspose.com>
2017-06-14 21:53:11 +0500

    New feature for Adding image in table and applying 3D rotation to shape
    AddImageInTableCell.java
    Add3DRotationEffectOnShape.java

---
+++ Examples/src/main/java/com/aspose/slides/examples/Slides/Shapes/Add3DRotationEffectOnShape.java
@@ -0,0 +1,52 @@
+package com.aspose.slides.examples.Slides.Shapes;
+
+import java.awt.Color;
+
+import com.aspose.slides.BevelPresetType;
+import com.aspose.slides.CameraPresetType;
+import com.aspose.slides.FillType;
+import com.aspose.slides.IAutoShape;
+import com.aspose.slides.ILineFillFormat;
+import com.aspose.slides.ISlide;
+import com.aspose.slides.LightRigPresetType;
+import com.aspose.slides.LightingDirection;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.ShapeType;
+import com.aspose.slides.examples.Utils;
+
+public class Add3DRotationEffectOnShape {
+
+	public static void main(String[] args) {
+                   //ExStart:Add3DRotationEffectOnShape 
+		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(Add3DRotationEffectOnShape.class);
+
+		// Create an instance of Presentation class
+		Presentation pres = new Presentation();
+		ISlide slide = pres.getSlides().get_Item(0);
+
+		// Add a shape on slide
+		IAutoShape shape = slide.getShapes().addAutoShape(ShapeType.Ellipse, 30, 30, 100, 100);
+		shape.getFillFormat().setFillType(FillType.Solid);
+		shape.getFillFormat().getSolidFillColor().setColor(Color.GREEN);
+		ILineFillFormat format = shape.getLineFormat().getFillFormat();
+		format.setFillType(FillType.Solid);
+		format.getSolidFillColor().setColor(Color.ORANGE);
+		shape.getLineFormat().setWidth(2.0);
+
+		// Set ThreeDFormat properties of shape
+		shape.getThreeDFormat().setDepth(4);
+		shape.getThreeDFormat().getBevelTop().setBevelType(BevelPresetType.Circle);
+		shape.getThreeDFormat().getBevelTop().setHeight(6);
+		shape.getThreeDFormat().getBevelTop().setWidth(6);
+		shape.getThreeDFormat().getCamera().setCameraType(CameraPresetType.OrthographicFront);
+		shape.getThreeDFormat().getLightRig().setLightType(LightRigPresetType.ThreePt);
+		shape.getThreeDFormat().getLightRig().setDirection(LightingDirection.Top);
+
+		// Write the presentation as a PPTX file
+		pres.save(dataDir + "Bavel.pptx", SaveFormat.Pptx);
+                //ExEnd:Add3DRotationEffectOnShape   
+	}
+
+}
---
+++ Examples/src/main/java/com/aspose/slides/examples/Slides/Table/AddImageInTableCell.java
@@ -0,0 +1,62 @@
+package com.aspose.slides.examples.Text;
+
+import com.aspose.slides.IAutoShape;
+import com.aspose.slides.IParagraph;
+import com.aspose.slides.IPortion;
+import com.aspose.slides.ISlide;
+import com.aspose.slides.ITextFrame;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.ShapeType;
+import com.aspose.slides.examples.Utils;
+
+public class AddImageInTableCell {
+
+	public static void main(String[] args) {
+                
+              //  ExStart:AddImageInTableCell
+
+		// The path to the documents directory.
+ String dataDir = Utils.getDataDir(AddImageInTableCell.class);
+
+  //Instantiate Presentation class object
+   Presentation pres = new Presentation();
+
+   //Access first slide
+   ISlide sld = pres.getSlides().get_Item(0);
+
+   //Define columns with widths and rows with heights
+   double[] dblCols = { 150, 150, 150, 150 };
+   double[] dblRows = { 100, 100, 100, 100, 90 };
+
+   //Add table shape to slide
+   ITable tbl = sld.getShapes().addTable(50, 50, dblCols, dblRows);
+
+   //Creating an Image object to hold the image file
+   IPPImage imgx = null;
+   try {
+           imgx = pres.getImages().addImage(new FileInputStream(new File("aspose1.jpg")));
+   } catch (IOException e) {
+   }
+
+   //Create an IPPImage object using the bitmap object
+   IPPImage imgx1 = pres.getImages().addImage(imgx);
+
+   //Add image to first table cell
+   tbl.get_Item(0, 0).getFillFormat().setFillType(FillType.Picture);
+   tbl.get_Item(0, 0).getFillFormat().getPictureFillFormat().setPictureFillMode(PictureFillMode.Stretch);
+   tbl.get_Item(0, 0).getFillFormat().getPictureFillFormat().setCropRight(-30f);
+   tbl.get_Item(0, 0).getFillFormat().getPictureFillFormat().setCropLeft(-30f);
+   tbl.get_Item(0, 0).getFillFormat().getPictureFillFormat().setCropBottom(-30f);
+   tbl.get_Item(0, 0).getFillFormat().getPictureFillFormat().setCropTop(-30f);
+   tbl.get_Item(0, 0).getFillFormat().getPictureFillFormat().getPicture().setImage(imgx);
+
+   //Save PPTX to Disk
+   pres.save("D:\\Data\\table.pptx", SaveFormat.Pptx);
+// ExEnd:AddImageInTableCell
+
+
+                
+	}
+
+}
