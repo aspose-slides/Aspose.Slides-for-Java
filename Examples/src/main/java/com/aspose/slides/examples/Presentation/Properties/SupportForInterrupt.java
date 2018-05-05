bc365832ccc13d85cf5c5814bdd1a809b67202f7
Muhammad-Adnan-Ahmad <adnan.ahmad@aspose.com>
2018-05-05 17:12:02 +0500

    added new feature as per Aspose.Slides for Java 18.4 release

---
+++ Examples/src/main/java/com/aspose/slides/examples/Presentation/Properties/SupportForInterrupt.java
@@ -0,0 +1,44 @@
+package com.aspose.slides.examples.Presentation.Properties;
+
+import com.aspose.slides.InterruptionTokenSource;
+import com.aspose.slides.LoadOptions;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import java.awt.Dimension;
+
+
+public class SupportForInterrupt {
+   
+    public static void main(String[] args) throws InterruptedException  { 
+     
+String dataDir = Utils.getDataDir(SupportForInterrupt.class);
+   //ExStart:SupportForInterrupt
+        
+   final InterruptionTokenSource tokenSource = new InterruptionTokenSource();
+ 
+   Runnable interruption = new Runnable() {
+   public void run() {
+   LoadOptions loadOptions = new LoadOptions();
+   loadOptions.setInterruptionToken(tokenSource.getToken().Clone());
+   Presentation pres = new Presentation("pres.pptx", loadOptions);
+try{
+   pres.getSlides().get_Item(0).getThumbnail(new Dimension(960, 720));
+   pres.save("pres.ppt", SaveFormat.Ppt);
+  }
+   finally {
+     pres.dispose();
+   }
+   }
+   };
+ 
+   Thread thread = new Thread(interruption);// run action in a separate thread
+   thread.start();
+ 
+   Thread.sleep(5000); // some work
+ 
+   tokenSource.interrupt();
+
+        //ExEnd:SupportForInterrupt
+
+   }
+}
