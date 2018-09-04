b50c5783dd55992684f1e1f3556e367c332c23ab
mudassirfayyaz <mudassir.fayyaz@aspose.com>
2018-09-04 20:04:40 +0500

    updated based on 18.8 release

--- Examples/src/main/java/com/aspose/slide/examples/ApplyLicense/MeteredLicensing.java
+++ Examples/src/main/java/com/aspose/slide/examples/ApplyLicense/MeteredLicensing.java
@@ -1,6 +1,8 @@
 package com.aspose.slide.examples.ApplyLicense;
 
 import java.math.BigDecimal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
@@ -12,20 +14,25 @@ public class MeteredLicensing {
 	
        com.aspose.slides.Metered metered=new com.aspose.slides.Metered();
      
-        // Access the setMeteredKey property and pass public and private keys as parameters
-        metered.setMeteredKey("<valid pablic key>", "<valid private key>");
-        
-        // Get consumed qantity value before accessing API
-            BigDecimal quantityOld = com.aspose.slides.Metered.getConsumptionQuantity();
+            try {
+                // Access the setMeteredKey property and pass public and private keys as parameters
+                metered.setMeteredKey("<valid pablic key>", "<valid private key>");
+           
+                   // Get consumed qantity value before accessing API
+            double quantityOld = com.aspose.slides.Metered.getConsumptionQuantity();
         System.out.println("Consumption quantity" + quantityOld);
 
-      
         
         // Get consumed qantity value after accessing API
-            BigDecimal quantity = com.aspose.slides.Metered.getConsumptionQuantity();
+            double quantity = com.aspose.slides.Metered.getConsumptionQuantity();
         System.out.println("Consumption quantity" + quantity());
 		
 
+            } catch (Exception ex) {
+                Logger.getLogger(MeteredLicensing.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+ 
 	}
 
     private static String quantity() {
