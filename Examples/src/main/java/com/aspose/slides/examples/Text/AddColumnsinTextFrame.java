23f00e904b0c50fc59fdd01f4b7e42421a829237
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-06-23 01:22:12 +0500

    Added new example

---
+++ Examples/src/main/java/com/aspose/slides/examples/Text/AddColumnsinTextFrame.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aspose.slides.examples.Text;
+
+import com.aspose.slides.AutoShape;
+import com.aspose.slides.IAutoShape;
+import com.aspose.slides.ISlide;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.ShapeType;
+import com.aspose.slides.TextFrameFormat;
+import com.aspose.slides.examples.Utils;
+
+/**
+ *
+ * @author mfazi
+ */
+public class AddColumnsinTextFrame {
+    
+    public static void main(String[] args) {
+                
+                 //ExStart:AddColumnsinTextFrame
+    		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(AddColumnsinTextFrame.class);
+                
+                String outPptxFileName = dataDir + "ColumnsTest.pptx";
+                
+                Presentation pres = new Presentation();
+                
+                // Get the first slide of presentation
+               ISlide slide = pres.getSlides().get_Item(0);
+                IAutoShape shape1 = slide.getShapes().addAutoShape(ShapeType.Rectangle, 100, 100, 300, 300);
+                TextFrameFormat format = (TextFrameFormat)shape1.getTextFrame().getTextFrameFormat();
+
+                format.setColumnCount(2);
+                shape1.getTextFrame().setText("All these columns are limited to be within a single text container -- " +
+                                          "you can add or delete text and the new or remaining text automatically adjusts " +
+                                          "itself to flow within the container. You cannot have text flow from one container " +
+                                          "to other though -- we told you PowerPoint's column options for text are limited!");
+                
+                pres.save(outPptxFileName, SaveFormat.Pptx);
+                
+               
+                //ExEnd:AddColumnsinTextFrame
+    }
+}
