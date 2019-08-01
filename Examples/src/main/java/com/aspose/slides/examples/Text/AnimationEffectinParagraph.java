5af457444922e084807f91565a96b181e9550a7c
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-08-01 10:03:15 +0500

    Added Example w.r.t 19.7

---
+++ Examples/src/main/java/com/aspose/slides/examples/Text/AnimationEffectinParagraph.java
@@ -0,0 +1,40 @@
+
+package com.aspose.slides.examples.Text;
+
+import com.aspose.slides.EffectSubtype;
+import com.aspose.slides.EffectTriggerType;
+import com.aspose.slides.EffectType;
+import com.aspose.slides.IAutoShape;
+import com.aspose.slides.IEffect;
+import com.aspose.slides.IParagraph;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.examples.Utils;
+
+
+public class AnimationEffectinParagraph {
+    public static void main(String[] args) {
+            
+        //ExStart:AnimationEffectinParagraph
+	// The path to the documents directory.		
+         String dataDir = Utils.getDataDir(AnimationEffectinParagraph.class);
+         
+         Presentation presentation = new Presentation(dataDir + "Presentation1.pptx");
+        try
+        {
+            // select paragraph to add effect
+            IAutoShape autoShape = (IAutoShape)presentation.getSlides().get_Item(0).getShapes().get_Item(0);
+            IParagraph paragraph = autoShape.getTextFrame().getParagraphs().get_Item(0);
+
+            // add Fly animation effect to selected paragraph
+            IEffect effect = presentation.getSlides().get_Item(0).getTimeline().getMainSequence().
+            addEffect(paragraph, EffectType.Fly, EffectSubtype.Left, EffectTriggerType.OnClick);
+            
+            presentation.save(dataDir + "AnimationEffectinParagraph.pptx", SaveFormat.Pptx);
+        } finally {
+            if (presentation != null) presentation.dispose();
+        }
+         //ExEnd:AnimationEffectinParagraph
+         
+    }
+}
