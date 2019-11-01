d0c36a5ccb19026c23a67598119a5bbb937592e1
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-11-01 11:58:53 +0500

    Added examples w.r.t 19.10

---
+++ Examples/src/main/java/com/aspose/slides/examples/Text/SetFontFallBack.java
@@ -0,0 +1,29 @@
+
+package com.aspose.slides.examples.Text;
+
+import com.aspose.slides.FontFallBackRule;
+import com.aspose.slides.IFontFallBackRule;
+import com.aspose.slides.examples.Utils;
+
+
+public class SetFontFallBack {
+    
+    public static void main(String[] args) {
+
+        //ExStart:SetFontFallBack 
+        
+        long startUnicodeIndex = 0x0B80;
+        long endUnicodeIndex = 0x0BFF;
+
+        IFontFallBackRule firstRule = new FontFallBackRule (startUnicodeIndex, endUnicodeIndex, "Vijaya");
+        IFontFallBackRule secondRule = new FontFallBackRule (0x3040, 0x309F, "MS Mincho, MS Gothic");
+
+        //Also the fonts list can be added in several ways:
+        String[] fontNames = new String[] { "Segoe UI Emoji, Segoe UI Symbol", "Arial"};
+
+        IFontFallBackRule thirdRule = new FontFallBackRule (0x1F300, 0x1F64F, fontNames);
+       
+        
+        //ExEnd:SetFontFallBack
+    }
+}
