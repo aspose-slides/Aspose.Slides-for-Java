8b4263140f9acd03a5fe89115ac1d24f3157aaef
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-12-22 11:20:03 +0500

    Added example w.r.t 19.12

---
+++ Examples/src/main/java/com/aspose/slides/examples/Presentation/Opening/SetAccessPermissionsToPDF.java
@@ -0,0 +1,32 @@
+
+package com.aspose.slides.examples.Presentation.Opening;
+
+import com.aspose.slides.PdfAccessPermissions;
+import com.aspose.slides.PdfOptions;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.examples.Utils;
+
+
+public class SetAccessPermissionsToPDF {
+    
+    public static void main(String[] args) {
+    
+        //ExStart:SetAccessPermissionsToPDF
+        // The path to the documents directory.
+        String dataDir = Utils.getDataDir(SetAccessPermissionsToPDF.class);
+        
+        PdfOptions pdfOptions = new PdfOptions();
+        pdfOptions.setPassword("my_password");
+        pdfOptions.setAccessPermissions(PdfAccessPermissions.PrintDocument| PdfAccessPermissions.HighQualityPrint);
+
+        Presentation presentation = new Presentation(dataDir+ "Presentation.pptx");
+        try {
+           presentation.save(dataDir+"PDFWithPermissions.pdf", SaveFormat.Pdf, pdfOptions);
+        } finally {
+           if (presentation != null) presentation.dispose();
+        }
+        //ExEnd:SetAccessPermissionsToPDF
+    }
+
+}
