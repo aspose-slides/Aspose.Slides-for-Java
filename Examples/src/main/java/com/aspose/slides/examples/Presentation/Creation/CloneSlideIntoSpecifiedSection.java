2a7a3853cf423595e027b9a42583dced6e0ce539
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-12-22 11:20:59 +0500

    Added example w.r.t 19.12

---
+++ Examples/src/main/java/com/aspose/slides/examples/Presentation/Creation/CloneSlideIntoSpecifiedSection.java
@@ -0,0 +1,33 @@
+
+package com.aspose.slides.examples.Presentation.Creation;
+
+import com.aspose.slides.IPresentation;
+import com.aspose.slides.ISection;
+import com.aspose.slides.Presentation;
+import com.aspose.slides.SaveFormat;
+import com.aspose.slides.ShapeType;
+import com.aspose.slides.examples.Utils;
+
+
+public class CloneSlideIntoSpecifiedSection {
+
+    public static final void main(String[] args) {
+        //ExStart:CloneSlideIntoSpecifiedSection
+        String dataDir = Utils.getDataDir(CloneSlideIntoSpecifiedSection.class);
+        IPresentation presentation = new Presentation();
+        try
+        {
+            presentation.getSlides().get_Item(0).getShapes().addAutoShape(ShapeType.Rectangle, 200, 50, 300, 100);
+            presentation.getSections().addSection("Section 1", presentation.getSlides().get_Item(0));
+
+            ISection section2 = presentation.getSections().appendEmptySection("Section 2");
+            presentation.getSlides().addClone(presentation.getSlides().get_Item(0), section2);
+
+            presentation.save(dataDir + "CloneSlideIntoSpecifiedSection.pptx", SaveFormat.Pptx);
+        } finally {
+            if (presentation != null) presentation.dispose();
+        }
+                
+     //ExEnd:CloneSlideIntoSpecifiedSection           
+    }
+}
