c4c5c8b20defe24ed6ca6fb25902c23e7b47a251
Olexiy.Shanhin <olexiy.shanhin@asposeptyltd.com>
2020-06-22 17:32:10 +0300

    fixed dispose pattern

--- Examples/src/main/java/com/aspose/slides/examples/shapes/ChangeShapeOrder.java
+++ Examples/src/main/java/com/aspose/slides/examples/shapes/ChangeShapeOrder.java
@@ -13,18 +13,25 @@ public class ChangeShapeOrder
         String dataDir = RunExamples.getDataDir_Shapes();
 
         Presentation presentation1 = new Presentation(dataDir + "HelloWorld.pptx");
-        ISlide slide = presentation1.getSlides().get_Item(0);
-        IAutoShape shp3 = slide.getShapes().addAutoShape(ShapeType.Rectangle, 200, 365, 400, 150);
-        shp3.getFillFormat().setFillType(FillType.NoFill);
-        shp3.addTextFrame(" ");
+        try
+        {
+            ISlide slide = presentation1.getSlides().get_Item(0);
+            IAutoShape shp3 = slide.getShapes().addAutoShape(ShapeType.Rectangle, 200, 365, 400, 150);
+            shp3.getFillFormat().setFillType(FillType.NoFill);
+            shp3.addTextFrame(" ");
 
-        ITextFrame txtFrame = shp3.getTextFrame();
-        IParagraph para = txtFrame.getParagraphs().get_Item(0);
-        IPortion portion = para.getPortions().get_Item(0);
-        portion.setText("Watermark Text Watermark Text Watermark Text");
-        shp3 = slide.getShapes().addAutoShape(ShapeType.Triangle, 200, 365, 400, 150);
-        slide.getShapes().reorder(2, shp3);
-        presentation1.save(dataDir + "Reshape_out.pptx", SaveFormat.Pptx);
+            ITextFrame txtFrame = shp3.getTextFrame();
+            IParagraph para = txtFrame.getParagraphs().get_Item(0);
+            IPortion portion = para.getPortions().get_Item(0);
+            portion.setText("Watermark Text Watermark Text Watermark Text");
+            shp3 = slide.getShapes().addAutoShape(ShapeType.Triangle, 200, 365, 400, 150);
+            slide.getShapes().reorder(2, shp3);
+            presentation1.save(dataDir + "Reshape_out.pptx", SaveFormat.Pptx);
+        }
+        finally
+        {
+            if (presentation1 != null) presentation1.dispose();
+        }
         //ExEnd:ChangeShapeOrder
     }
 }
